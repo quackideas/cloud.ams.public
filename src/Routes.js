df0c4b66203a5265842e47d8e9c960ba201387fe
quackideas <quannguyen@Quans-MacBook-Pro.local>
2018-03-21 15:28:50 -0700

    update for routes

--- src/Routes.js
+++ src/Routes.js
@@ -12,9 +12,9 @@ import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
 
 export default ({ childProps }) =>
   <Switch>
-    <AppliedRoute path="/" exact component={Home} props={childProps} />
-    <UnauthenticatedRoute path="/login" exact component={Login} props={childProps} />
-    <UnauthenticatedRoute path="/signup" exact component={Signup} props={childProps} />
+    <AppliedRoute path="/cloud-ams-public/index.html" exact component={Home} props={childProps} />
+    <UnauthenticatedRoute path="/cloud-ams-public/login" exact component={Login} props={childProps} />
+    <UnauthenticatedRoute path="/cloud-ams-public/signup" exact component={Signup} props={childProps} />
     <AuthenticatedRoute path="/notes/new" exact component={NewNote} props={childProps} />
     <AuthenticatedRoute path="/notes/:id" exact component={Notes} props={childProps} />
     { /* Finally, catch all unmatched routes */ }
